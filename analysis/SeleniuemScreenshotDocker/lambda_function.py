1bd847289f7fc1e655e58c0997167bf0b6a9084b
s2744007 <t.p.angevare@student.utwente.nl>
2025-01-18 23:49:12 +0100

    Started implementation of watermark

--- analysis/SeleniuemScreenshotDocker/lambda_function.py
+++ analysis/SeleniuemScreenshotDocker/lambda_function.py
@@ -5,13 +5,14 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from io import BytesIO
 import base64
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image
 
 def handler(event, context=None):
     url = event['url']
     watermark = True
     try:
         if event['watermark'] == 'False':
+            print('hello')
             watermark = False
     except:
         pass
@@ -31,30 +32,32 @@ def handler(event, context=None):
     options.add_argument(f"--user-data-dir={mkdtemp()}")
     options.add_argument(f"--data-path={mkdtemp()}")
     options.add_argument(f"--disk-cache-dir={mkdtemp()}")
-    options.add_argument("--window-size=1280x1696")
+    options.add_argument("--window-size=1280x3057")
     options.add_argument("--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36")
 
     driver = webdriver.Chrome(options=options, service=service)
     screenshot = take_screenshot(driver, url, "title", 10)
     driver.quit()
-
     if watermark:
         return add_watermark(screenshot)
     else:
-        return screenshot
+        return base64.b64encode(screenshot)
 
 def add_watermark(screenshot):
     buffered = BytesIO()
     image = Image.open(BytesIO(screenshot))
+    watermark = Image.open('Watermark.png')
 
 # Initialize the drawing context with the image as background
-    watermark_text = 'Chatalytics'
-    text_position = (100, 100)
-    font = ImageFont.load_default()
-    draw = ImageDraw.Draw(image)
-    draw.text(text_position, watermark_text, font=font, fill=(255, 255, 255))
+    watermark_size = (1280, 3057)
+    watermark = watermark.resize(watermark_size)
 
-    image.save(buffered, format="PNG")
+# Paste the watermark onto the image
+    transparent = Image.new('RGBA', image.size)
+    transparent.paste(image, (0, 0))
+    transparent.paste(watermark, (0,0), mask=watermark)
+    transparent = transparent.convert('RGB')
+    transparent.save(buffered, format="PNG")
 
     return base64.b64encode(buffered.getvalue())
 
@@ -62,5 +65,5 @@ def take_screenshot(driver, url, element_id, timeout):
     driver.get(url)
      
     element = WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.ID, element_id)))
-        
+    driver.set_window_size(1280, 3057)
     return driver.get_screenshot_as_png()
